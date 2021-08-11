59a64af6f12c862af9d8bbe222a6d9ac5eb16bbb
hamzaig <hamzaig@yahoo.com>
2021-08-11 22:29:29 +0500

    Git Learning Starts

---
+++ AllCommands.js
@@ -0,0 +1,13 @@
+			//1. Some Basic Commands to Push Repository to GitHub
+
+//git init
+//git add README.md
+//git commit -m "first commit"
+//git branch -M main
+//git remote add origin git@github.com:{username}/{repositoryName}.git
+//git push -u origin main
+
+// push an existing repository from the command line
+//git remote add origin git@github.com:{username}/{repositoryName}.git
+//git branch -M main
+//git push -u origin main
